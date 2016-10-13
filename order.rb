a155302273f9beb84b0bf7129966de605f269d20
TheDG <dsinay@uc.cl>
2016-10-13 17:08:59 -0300

    Fix Display

--- order.rb
+++ order.rb
@@ -11,36 +11,42 @@ class Order
     @beverage_list << beverage
   end
 
-  def print
-    @beverage_list.each do |bev|
-      puts bev.type
-      puts 'precio ' + bev.price.to_s
-      puts 'tiempo ' + bev.time.to_s
+  def special_condiment(bev)
+    extra_price = 0
+    extra_price -= 200 if bev.contains_whole_milk
+    extra_price += 300 if bev.contains_soy_milk
+    if bev.contains_ice_cream
+      extra_price += if rand < 0.5
+                       100
+                     else
+                       500
+                     end
     end
+    if bev.contains_chocolate_condiment
+      extra_price += 200 unless bev.chocolate?
+    end
+    extra_price
+  end
+
+  def print
     puts '******************************************'
     puts 'RESUMEN ORDEN'
     puts name_to_show
+    @beverage_list.each do |bev|
+      puts "Beverge: #{bev.type}"
+      puts "  Price: #{bev.price + special_condiment(bev)}"
+      puts "  Time: #{bev.time}"
+    end
+    puts "El precio total es: $  #{@total_price}"
+    puts "El tiempo de preparacion es:  #{calculate_total_time} segundos"
     puts '******************************************'
-    puts 'El precio total es: $' + @total_price.to_s
-    puts 'El tiempo de preparacion es: ' + calculate_total_time.to_s + ' segundos'
   end
 
   def calculate_total_price
     @total_price = 0
     @beverage_list.each do |bev|
       @total_price += bev.price
-      @total_price -= 200 if bev.contains_whole_milk
-      @total_price += 300 if bev.contains_soy_milk
-      if bev.contains_ice_cream
-        @total_price += if rand < 0.5
-                          100
-                        else
-                          500
-                        end
-      end
-      if bev.contains_chocolate_condiment
-        @total_price += 200 unless bev.chocolate?
-      end
+      @total_price += special_condiment(bev)
     end
   end
 
